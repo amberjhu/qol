52c26bce193268d81c50187557b5e68ea0e3ecd9
Amber Hu <amber@amberjhu.org>
2026-03-29 21:17:51 -0700

    Initial setup script

---
+++ setup.sh
@@ -0,0 +1,69 @@
+#!/bin/bash
+# Copyright 2026 Amber Hu
+echo "This is a script to help me set up Linux environments. Provided as is."
+read -p "Would you like me to overwrite ~/.vimrc file? [y/N] " OVERWRITE_VIM
+# Use ${VAR_NAME,,} to expand to all lowercase string and ^^ for all uppercase
+if [[ ${OVERWRITE_VIM,,} == 'y' ]]; then
+  echo "Copying to ~/.vimrc"
+  cp ./vimrc ~/.vimrc
+fi
+read -p "Would you like me to overwrite ~/.bashrc file? [y/N] " OVERWRITE_BASHRC
+if [[ ${OVERWRITE_BASHRC,,} == 'y' ]]; then  
+  echo "copying to ~/.bashrc"
+  cp ./bashrc ~/.bashrc
+fi
+
+read -p "Which package manager does this Linux distro use? (A)pt, (D)nf, (P)acman " PACKAGE_MANAGER_REPLY
+if [[ ${PACKAGE_MANAGER_REPLY,,} == 'a' ]]; then
+  PACKAGE_MANAGER='apt'
+elif [[ ${PACKAGE_MANAGER_REPLY,,} == 'd' ]]; then
+  PACKAGE_MANAGER='dnf'
+elif [[ ${PACKAGE_MANAGER_REPLY,,} == 'p' ]]; then
+  PACKAGE_MANAGER='pacman'
+else
+  echo "Could not recognize prompt response, quitting"
+  exit 1
+fi
+echo "Using package manager $PACKAGE_MANAGER"
+
+check_installed() {
+  if [[ $PACKAGE_MANAGER == 'apt' ]]; then
+    apt list --installed $1 | grep "installed"
+    return $?
+  elif [[ $PACKAGE_MANAGER == 'dnf' ]]; then
+    dnf list installed $1
+    return $?
+  else
+    echo "Not implemented!"
+    exit 67
+  fi
+}
+
+install_package() {
+  if [[ $PACKAGE_MANAGER == 'apt' ]]; then
+    sudo apt install $1
+    return $?
+  elif [[ $PACKAGE_MANAGER == 'dnf' ]]; then
+    sudo dnf install $1
+    return $?
+  else
+    echo "Not implemented!"
+    exit 67
+  fi
+}
+
+check_and_install() {
+  echo "Installing $1..."
+  check_installed $1 || install_package $1
+}
+
+ESSENTIAL_PACKAGES="git curl wget vim"
+echo "Installing the essentials: $ESSENTIAL_PACKAGES"
+for package in $ESSENTIAL_PACKAGES; do
+  check_and_install $package
+done
+
+read -p "Would you like to generate SSH keys? [y/N] " GEN_SSH_KEYS
+if [[ ${GEN_SSH_KEYS,,} == 'y' ]]; then
+  exit 67
+fi
